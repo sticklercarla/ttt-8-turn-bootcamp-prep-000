33d7fcaf5a61922fb90744a2f04986f21af36588
Carla Stickler <carlastickler@gmail.com>
2019-09-18 00:30:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -6,5 +5,5 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  binding.pry
+
 end
