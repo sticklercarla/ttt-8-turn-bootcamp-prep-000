3160d2ae4b14a520a4f5770ede1c4a64c8f3b92a
Carla Stickler <carlastickler@gmail.com>
2019-09-18 00:31:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,5 +8,9 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
 end
